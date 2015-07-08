909f5d8c6e562208ed679b61895ecf1d328d7868
Neil Kim Gardose <nkpgardose@gmail.com>
2015-07-08 22:07:10 +0800

    the burger is real on this one

--- js/burgers.js
+++ js/burgers.js
@@ -1,5 +1,7 @@
 $(function() {
   $('.burger--bouncy.js-burger').click(function() { $(this).toggleClass('is-active');});
   $('.burger--back.js-burger').click(function() { $(this).toggleClass('is-active');});
+  $('.burger--realWithCheese.js-burger').click( function() {
+    $(this).toggleClass('is-active');
+  });
 });
-
